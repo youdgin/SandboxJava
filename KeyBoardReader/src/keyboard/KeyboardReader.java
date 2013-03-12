fd06f4a7025a853474e5ed870aa20aa1497bdebb
youdgin <youdgin@gmail.com>
2013-03-12 13:24:26 +0200

    Initial void project.

---
+++ KeyBoardReader/src/keyboard/KeyboardReader.java
@@ -0,0 +1,17 @@
+package keyboard;
+
+public class KeyboardReader {
+
+	public KeyboardReader() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
