a29c6c36b50517180a613e965b0ba7fb91fd07c4
youdgin <youdgin@gmail.com>
2013-03-12 13:27:53 +0200

    First crude implementation of reading from keyboard using main function

--- KeyBoardReader/src/keyboard/KeyboardReader.java
+++ KeyBoardReader/src/keyboard/KeyboardReader.java
@@ -1,17 +1,24 @@
 package keyboard;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class KeyboardReader {
 
 	public KeyboardReader() {
-		// TODO Auto-generated constructor stub
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			System.out.println(bufferedReader.readLine());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
