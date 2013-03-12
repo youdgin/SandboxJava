2b989e79f7698c129c6797bf5089475125acee12
youdgin <youdgin@gmail.com>
2013-03-12 14:02:19 +0200

    A little more OOP approach of reading from file.

--- KeyBoardReader/src/file/FileTextReader.java
+++ KeyBoardReader/src/file/FileTextReader.java
@@ -6,34 +6,30 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class FileTextReader {
-
-	public FileTextReader() {
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		File file = new File("Text.txt");
-		FileReader fileReader = null;
+	File file;
+	FileReader fileReader;
+	char[] cbuf;
+	public FileTextReader(String pathname) {
+		file = new File(pathname);
 		try {
 			fileReader = new FileReader(file);
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			System.exit(1);
 		}
 //		Warning! not too big file here to not flip the int limits, need to rethink this
-		char[]  cbuf = new char[(int)file.length()];
+		cbuf = new char[(int)file.length()];
+	}
+	
+	public char[] fileRead(){
 		try {
 			fileReader.read(cbuf);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println(cbuf);
+		return cbuf;
 	}
+
 
 }
--- KeyBoardReader/src/main/Main.java
+++ KeyBoardReader/src/main/Main.java
@@ -1,12 +1,17 @@
 package main;
 
+import file.FileTextReader;
 import keyboard.KeyboardReader;
 
 public class Main {
 
 	public static void main(String[] args) {
 		KeyboardReader keyboardReader = new KeyboardReader();
-		System.out.println(keyboardReader.readLine());
+		
+		System.out.println("Please enter the filename you wana read from");
+		
+		FileTextReader fileTextReader = new FileTextReader(keyboardReader.readLine());
+		System.out.println(fileTextReader.fileRead());
 	}
 	
 	
